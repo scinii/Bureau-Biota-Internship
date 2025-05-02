e36b65aa23be9ef1396dab8f4c5a9809566d8558
scinii <roberto.schinina02@gmail.com>
2025-05-02 22:40:12 +0200

    comment cca_plot

--- data_analysis.R
+++ data_analysis.R
@@ -149,6 +149,14 @@ cca_data <- function(year_code){
 
 cca_plot <- function(year_code, rhs_formula_string){
   
+  # Description: This function performs cca and make the ordination plot
+  #              
+  # Args:
+  #       year_code: which year you want to consider
+  #       rhs_formula_string: the right hand side of the formula (i.e. which
+  #       environmental variables you want to use)
+  # Return: cca object of the vegan package.
+  
   data <- cca_data(year_code)
   data_non_env <- data[[1]]
   data_env <- data[[2]]
@@ -158,7 +166,7 @@ cca_plot <- function(year_code, rhs_formula_string){
   analysis <- cca(formula_cca, data = data_env)
   
   plot(analysis)
-  
+  return(analysis)
 }
 
 
